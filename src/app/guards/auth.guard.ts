8345fc302388eb43b9570eb16713c5f4ea733928
DEVARSHI-GOHEL <97431104+DEVARSHI-GOHEL@users.noreply.github.com>
2022-03-22 20:13:59 +0530

    Added guards and models

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private auth: AuthenticationService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+    if (this.auth.isLoggedIn !== true) {
+      this.router.navigate(['login']);
+      return false;
+    }
+
+    return true;
+  }
+
+}
---
+++ src/app/models/products.model.ts
@@ -0,0 +1,7 @@
+export interface Products {
+    name: string,
+    img: string,
+    price: string,
+    quantity: string,
+    category: string
+}
